563e66db5738dffca8914223221111fd3e48afa4
Michael Janssen <m.janssen@lyrah.net>
2026-08-07 16:24:36 +0200

    Version 1.8-0 : config as argument possible

--- aurakey.sh
+++ aurakey.sh
@@ -4,21 +4,39 @@
 # Author: Michael Janssen <m.janssen@lyrah.net>
 # License: GPLv3 (See README.md for details)
 
-VERSION="1.7-6"
+VERSION="1.8-0"
 TRIES=0 # needs to be zero to start the loop
 
-# search for external config and load it
-{ source ./aurakey.cfg || source /etc/aurakey.cfg || source /usr/local/etc/aurakey.cfg ; } 2>/dev/null || echo "Warning: No config file found!"
+# check for config argument
+if [[ "$1" == "--decrypt" ]]
+then
+	if [[ -n "$2" && -f "$2" ]]
+	then
+		CONFIG_FILE="$2"
+	elif [[ -n "$3" && -f "$3" ]]
+	then
+		CONFIG_FILE="$3"
+	fi
+
+	# search for external config and load it
+	if [[ -n "$CONFIG_FILE" ]]
+	then
+		source "$CONFIG_FILE"
+	else
+		# else try default config filenames
+		{ source ./aurakey.cfg || source /etc/aurakey.cfg || source /usr/local/etc/aurakey.cfg ; } 2>/dev/null || echo "Warning: No config file found!"
+	fi
+fi
 
 if [[ "$2" == "--silent" ]]
-	then
-		VERBOSE_MODE="0"
-	elif [[ "$2" == "--verbose" ]]
+then
+	VERBOSE_MODE="0"
+elif [[ "$2" == "--verbose" ]]
 	then
 		VERBOSE_MODE="2"
 	else
 		VERBOSE_MODE="1"
-	fi
+fi
 
 	# only show if not in silent mode
 	if [[ "$VERBOSE_MODE" != "0" ]]
@@ -409,7 +427,7 @@ then
 	echo "Usage: $0 [OPTION] [ARGUMENTS]"
 	echo ""
 	echo "Options:"
-	echo "   --decrypt [ --silent | --verbose ]" "Start the decryption process and mount the drive."
+	echo "   --decrypt [ --silent | --verbose ] <optional-config-file>" "Start the decryption process and mount the drive."
 	echo "   --create-keyfile [path]" "Generate a new 4KB random key and encrypt it via GPG."
 	echo "   --hide-keyfile <key> <img_in> <img_out>" "Hide a GPG-keyfile inside a JPG image."
 	echo "   --add-keyfile-to-drive <img_key>" "Add the hidden key from an image to a LUKS slot."
